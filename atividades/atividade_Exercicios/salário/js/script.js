e380885a7110929b9efdc44fcb7019bca81a5525
user <teste>
2025-03-19 09:00:42 -0300

    first commit

---
+++ atividades/atividade_Exercicios/salário/js/script.js
@@ -0,0 +1,32 @@
+function salarios() {
+    mensagemBruto = ""
+    //mensagem vazia//
+
+    valorDaHora = parseFloat(document.getElementById("tarifa por hora").value);
+    horasTrabalhadas = parseFloat(document.getElementById("numero de horas").value);
+    salarioBruto = (valorDaHora * horasTrabalhadas);
+    //valor do salário bruto//
+
+    mensagemBruto = `tu salario bruto es ${salarioBruto}`;
+    document.getElementById("salario bruto").innerHTML = mensagemBruto;
+    //mensagem com a indicação do salario bruto//
+
+    mensagemNeto = "";
+    //mensagem vazia//
+
+    if (salarioBruto <= 5000) {
+        imposto = salarioBruto * 0.15;
+        salarioNeto = salarioBruto - imposto;
+        mensagemNeto = `tu salario neto es ${salarioNeto}`;
+    }
+    //Salario liquido com 15%//
+
+    if (salarioBruto > 5000) {
+        imposto = salarioBruto * 0.22;
+        salarioNeto = salarioBruto - imposto;
+        mensagemNeto = `tu salario neto es ${salarioNeto}`;
+    }
+    //salario liquido com 22%//
+
+    document.getElementById("salario neto").innerHTML = mensagemNeto;
+}
