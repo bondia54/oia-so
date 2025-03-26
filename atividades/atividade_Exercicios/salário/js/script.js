e10b3ffb3010a0a8825014cab67d3a44e794d96b
Vinicius <diasaraujovinicius@gmail.com>
2025-03-26 09:14:59 -0300

    26/03

--- atividades/atividade_Exercicios/salário/js/script.js
+++ atividades/atividade_Exercicios/salário/js/script.js
@@ -1,33 +1,34 @@
 function salarios() {
-    mensagemBruto = ""
+    mensagemBruto = "";
     //mensagem vazia//
 
     valorDaHora = parseFloat(document.getElementById("tarifa por hora").value);
     horasTrabalhadas = parseFloat(document.getElementById("numero de horas").value);
-    salarioBruto = (valorDaHora * horasTrabalhadas);
+    salarioBruto = (valorDaHora * horasTrabalhadas).toFixed(2);
     //valor do salário bruto//
 
-    mensagemBruto = `tu salario bruto es ${salarioBruto}`;
+    mensagemBruto = `tu salario bruto es R$${salarioBruto}`;
     document.getElementById("salario bruto").innerHTML = mensagemBruto;
     //mensagem com a indicação do salario bruto//
 
     mensagemNeto = "";
     //mensagem vazia//
 
-    if (salarioBruto <= 5000) {
+    if (salarioBruto <= 1500) {
+        imposto = 0;
+    } else if (salarioBruto <= 3000) {
+        imposto = salarioBruto * 0.1;
+    } else if (salarioBruto <= 5000) {
         imposto = salarioBruto * 0.15;
-        salarioNeto = salarioBruto - imposto;
-        mensagemNeto = `tu salario neto es ${salarioNeto}`;
-    }
-    //Salario liquido com 15%//
-
-    if (salarioBruto > 5000) {
+    } else if (salarioBruto <= 10000) {
         imposto = salarioBruto * 0.22;
-        salarioNeto = salarioBruto - imposto;
-        mensagemNeto = `tu salario neto es ${salarioNeto}`;
+    } else {
+        imposto = salarioBruto * 0.27;
     }
-    //salario liquido com 22%//
+    //calculo do salario neto//
 
+    salarioNeto = salarioBruto - imposto.toFixed(2);
+    mensagemNeto = `tu salario neto es R$${salarioNeto}`;
     document.getElementById("salario neto").innerHTML = mensagemNeto;
     //mensagem com a indicação do salario líquido//
 }
